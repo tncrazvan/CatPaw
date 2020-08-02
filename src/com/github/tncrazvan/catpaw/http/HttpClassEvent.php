0c8c7815ddfcdc8198918c7025798e788c716118
Razvan - Home  - WSL <tangent.jotey@gmail.com>
2020-08-02 22:03:43 +0200

    update

--- src/com/github/tncrazvan/catpaw/http/HttpClassEvent.php
+++ src/com/github/tncrazvan/catpaw/http/HttpClassEvent.php
@@ -1,11 +1,3 @@
 <?php
 namespace com\github\tncrazvan\catpaw\http;
-abstract class HttpClassEvent{
-    private $method;
-    public function __construct(string &$method){
-        $this->method = $method;
-    }
-    public function &getMethod():string{
-        return $this->method;
-    }
-}
+abstract class HttpClassEvent{}
