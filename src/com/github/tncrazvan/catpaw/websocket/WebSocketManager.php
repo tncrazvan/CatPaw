54e56b5f8e8cd528562238e98ad849ccd8810011
Razvan - Home  - WSL <tangent.jotey@gmail.com>
2020-08-04 20:57:31 +0200

    reformed websocket, not finished yet

--- src/com/github/tncrazvan/catpaw/websocket/WebSocketManager.php
+++ src/com/github/tncrazvan/catpaw/websocket/WebSocketManager.php
@@ -69,24 +69,216 @@ abstract class WebSocketManager extends EventManager{
             $this->listener->so->wsMtu = 65535;
         $mtu = $this->listener->so->wsMtu;
         
-        $masked = \fread($this->listener->client, $mtu);
+        $masked = \stream_get_contents($this->listener->client, -1);
         if ($masked === false) {
             $this->close();
             //$this->listener->so->websocketConnections->deleteNode($this);
             unset($this->listener->so->websocketConnections[$this->requestId]);
             $this->uninstall();
         } else {
+            if($masked === '') return;
+            $start = \round(microtime(true) * 1000);
             $unpacked = \unpack("C*", $masked);
             $length = \count($unpacked);
-            $start = \round(microtime(true) * 1000000);
-            for($i=1;$i<=$length;$i++){
-                $this->unmask($unpacked[$i]);
-            }
-            $end = \round(microtime(true) * 1000000);
+            $end = \round(microtime(true) * 1000);
             $t = $end - $start;
-            echo ("it took $t micros to unpack and unmask.\n");
+            echo ("it took $t ms to unpack and count.\n");
+            
+            $start = (\round(microtime(true) * 1000));
+            $this->unmask($unpacked);
+            echo ("it took ".(\round(microtime(true) * 1000) - $start)." ms.\n##########################################\n");
         }
 
+    }
+
+    private const FIRST_BYTE = 0, SECOND_BYTE = 1, LENGTH2 = 2, LENGTH8 = 3, MASK = 4, PAYLOAD = 5, DONE = 6;
+    private int $lengthKey = 0, $reading = self::FIRST_BYTE, $lengthIndex = 0, $maskIndex = 0, $payloadIndex = 0,
+            $payloadLength = 0;
+    // private boolean fin,rsv1,rsv2,rsv3;
+    private $opcode;
+    private /*?array*/ $payload = null, $mask = null, $length = null;
+    private bool $isContinuation = false;
+    private bool $isFinal = false;
+    private array $listOfPayloads = [];
+    
+    private $startTime = 0;
+    private $endTime;
+
+
+    private function unmask(array &$unpacked,int $shift = 0){
+        if($this->reading === self::PAYLOAD){
+            $this->unmask_payload($unpacked);
+        }else{
+            $this->unmask_first_byte($unpacked[1+$shift]);
+            $this->unmask_second_byte($unpacked[2+$shift]);
+            switch ($this->reading) {
+                case self::MASK:
+                    $this->unmask_mask([
+                        $unpacked[3+$shift],$unpacked[4+$shift],
+                        $unpacked[5+$shift],$unpacked[6+$shift]
+                    ]);
+                    $this->unmask_payload($unpacked,7+$shift);
+                break;
+                case self::LENGTH2:
+                    $this->unmask_length2([$unpacked[3+$shift],$unpacked[4+$shift]]);
+                    $this->unmask_mask([
+                        $unpacked[5+$shift],$unpacked[6+$shift],
+                        $unpacked[7+$shift],$unpacked[8+$shift]
+                    ]);
+                    $this->unmask_payload($unpacked,9+$shift);
+                break;
+                case self::LENGTH8:
+                    $this->unmask_length8([
+                        $unpacked[3+$shift],$unpacked[4+$shift],
+                        $unpacked[5+$shift],$unpacked[6+$shift],
+                        $unpacked[7+$shift],$unpacked[8+$shift],
+                        $unpacked[9+$shift],$unpacked[10+$shift]
+                    ]);
+                    $this->unmask_mask([
+                        $unpacked[11+$shift],$unpacked[12+$shift],
+                        $unpacked[13+$shift],$unpacked[14+$shift]
+                    ]);
+                    $this->unmask_payload($unpacked,15+$shift);
+                break;
+            }
+        }
+    }
+
+    private function unmask_first_byte(int $b):void{
+        //$start = round(microtime(true) * 100000);
+        $this->isFinal = (($b & 0x80) !== 0);
+        // rsv1 = ((b & 0x40) != 0);
+        // rsv2 = ((b & 0x20) != 0);
+        // rsv3 = ((b & 0x10) != 0);
+        if($this->startTime === 0)
+            $this->startTime = round(microtime(true) * 100000);
+        
+        $this->opcode = ($b & 0x0F);
+        $this->isContinuation = $this->opcode === 0;
+        if ($this->opcode == 0x8) { // fin
+            $this->close();
+            //$this->listener->so->websocketConnections->deleteNode($this);
+            unset($this->listener->so->websocketConnections[$this->requestId]);
+            $this->uninstall();
+        }
+        $this->mask = [null,null,null,null];
+        $this->reading = self::SECOND_BYTE;
+        //$end = round(microtime(true) * 100000);
+        //echo "FIRST_BYTE took ".($end - $start)."ms\n";
+    }
+
+    private function unmask_second_byte(int $b):void{
+        //$start = round(microtime(true) * 100000);
+        $this->lengthKey = $b & 127;
+        if ($this->lengthKey <= 125) {
+            $this->length = [null];
+            $this->length[0] = $this->lengthKey;
+            $this->payloadLength = $this->lengthKey & 0xff;
+            $this->reading = self::MASK;
+        } else if ($this->lengthKey == 126) {
+            $this->reading = self::LENGTH2;
+            $this->length = [null,null];
+        } else if ($this->lengthKey == 127) {
+            $this->reading = self::LENGTH8;
+            $this->length = [null,null,null,null,null,null,null,null];
+        }
+        //$end = round(microtime(true) * 100000);
+        //echo "SECOND_BYTE took ".($end - $start)."ms\n";
+    }
+
+    private function unmask_length2(array $b):void{
+        //$start = round(microtime(true) * 100000);
+        $l = count($b);
+        for($j=0;$j<$l;$j++){
+            $this->length[$this->lengthIndex] = $b[$j];
+            $this->lengthIndex++;
+            if ($this->lengthIndex === 2) {
+                $this->payloadLength = (($this->length[0] & 0xff) << 8) | ($this->length[1] & 0xff);
+                $this->reading = self::MASK;
+            }
+        }
+        
+        //$end = round(microtime(true) * 100000);
+        //echo "LENGTH2 took ".($end - $start)."ms\n";
+    }
+
+    private function unmask_length8(array $b):void{
+        //$start = round(microtime(true) * 100000);
+        $l = count($b);
+        for($j=0;$j<$l;$j++){
+            $this->length[$this->lengthIndex] = $b[$j];
+            $this->lengthIndex++;
+            if ($this->lengthIndex === 8) {
+                $this->payloadLength = $this->length[0] & 0xff;
+                $length_length = count($this->length);
+                for ($i = 1; $i < $length_length; $i++) {
+                    $this->payloadLength = (($this->payloadLength) << 8) | ($this->length[$i] & 0xff);
+                }
+                $this->reading = self::MASK;
+            }
+        }
+        //$end = round(microtime(true) * 100000);
+        //echo "LENGTH8 took ".($end - $start)."ms\n";
+    }
+
+    private function unmask_mask(array $b):void{
+        //$start = round(microtime(true) * 100000);
+        for($j=0;$j<4;$j++){
+            $this->mask[$this->maskIndex] = $b[$j];
+            $this->maskIndex++;
+            if ($this->maskIndex === 4) {
+                $this->reading = self::PAYLOAD;
+                // int l = (int)ByteBuffer.wrap(length).getLong();
+                $this->payload = array_fill(0,$this->payloadLength,null);
+            }
+        }
+        //$end = round(microtime(true) * 100000);
+        //echo "MASK took ".($end - $start)."ms\n";
+    }
+
+    private function unmask_payload(array &$b,int $offset = 1):void{
+        $l=count($b);
+        for($j=$offset;$j<=$l;$j++){
+            if ($this->payloadLength === 0) {
+                $this->unmask($b,$j-1);
+                return;
+            }
+
+            $this->payload[$this->payloadIndex] = chr(($b[$j] ^ $this->mask[($this->payloadIndex) % 4]));
+
+
+            $this->payloadIndex++;
+            
+
+            
+            if ($this->payloadIndex === $this->payloadLength) {
+                
+                $this->reading = self::DONE;
+
+                if(null !== $this->onMessage){
+
+                    $payload = implode('',$this->payload);
+                    $this->listOfPayloads[] = $payload;
+                    if( $this->isFinal){
+                        $res = implode('',$this->listOfPayloads);
+                        $this->listOfPayloads = [];
+                        $this->startTime = 0;
+                        $this->onMessage->run($res);
+                    }
+                    
+                }
+                $this->lengthKey = 0;
+                $this->reading = self::FIRST_BYTE;
+                $this->lengthIndex = 0;
+                $this->payloadLength = 0;
+                $this->maskIndex = 0;
+                $this->payloadIndex = 0;
+                $this->payload = null;
+                $this->mask = null;
+                $this->length = null;
+            }
+            
+        }
     }
     
     /*
@@ -112,170 +304,6 @@ abstract class WebSocketManager extends EventManager{
              |                     Payload Data continued ...                |
              +---------------------------------------------------------------+
         */
-
-
-    private const FIRST_BYTE = 0, SECOND_BYTE = 1, LENGTH2 = 2, LENGTH8 = 3, MASK = 4, PAYLOAD = 5, DONE = 6;
-    private int $lengthKey = 0, $reading = self::FIRST_BYTE, $lengthIndex = 0, $maskIndex = 0, $payloadIndex = 0,
-            $payloadLength = 0;
-    // private boolean fin,rsv1,rsv2,rsv3;
-    private $opcode;
-    private /*?array*/ $payload = null, $mask = null, $length = null;
-    private bool $isContinuation = false;
-    private bool $isFinal = false;
-    private array $listOfPayloads = [];
-    
-    private $startTime = 0;
-    private $endTime;
-
-    private function unmask(int $b){
-        switch ($this->reading) {
-            case self::FIRST_BYTE:
-                $start = round(microtime(true) * 100000);
-                $this->isFinal = (($b & 0x80) !== 0);
-                // rsv1 = ((b & 0x40) != 0);
-                // rsv2 = ((b & 0x20) != 0);
-                // rsv3 = ((b & 0x10) != 0);
-                if($this->startTime === 0)
-                    $this->startTime = round(microtime(true) * 100000);
-                
-                $this->opcode = ($b & 0x0F);
-                $this->isContinuation = $this->opcode === 0;
-                if ($this->opcode == 0x8) { // fin
-                    $this->close();
-                    //$this->listener->so->websocketConnections->deleteNode($this);
-                    unset($this->listener->so->websocketConnections[$this->requestId]);
-                    $this->uninstall();
-                }
-                $this->mask = [null,null,null,null];
-                $this->reading = self::SECOND_BYTE;
-                $end = round(microtime(true) * 100000);
-                echo "FIRST_BYTE took ".($end - $start)."ms\n";
-            break;
-            case self::SECOND_BYTE:
-                $start = round(microtime(true) * 100000);
-                $this->lengthKey = $b & 127;
-                if ($this->lengthKey <= 125) {
-                    $this->length = [null];
-                    $this->length[0] = $this->lengthKey;
-                    $this->payloadLength = $this->lengthKey & 0xff;
-                    $this->reading = self::MASK;
-                } else if ($this->lengthKey == 126) {
-                    $this->reading = self::LENGTH2;
-                    $this->length = [null,null];
-                } else if ($this->lengthKey == 127) {
-                    $this->reading = self::LENGTH8;
-                    $this->length = [null,null,null,null,null,null,null,null];
-                }
-                $end = round(microtime(true) * 100000);
-                echo "SECOND_BYTE took ".($end - $start)."ms\n";
-            break;
-            case self::LENGTH2:
-                $start = round(microtime(true) * 100000);
-                $this->length[$this->lengthIndex] = $b;
-                $this->lengthIndex++;
-                if ($this->lengthIndex === 2) {
-                    $this->payloadLength = (($this->length[0] & 0xff) << 8) | ($this->length[1] & 0xff);
-                    $this->reading = self::MASK;
-                }
-                $end = round(microtime(true) * 100000);
-                echo "LENGTH2 took ".($end - $start)."ms\n";
-            break;
-            case self::LENGTH8:
-                $start = round(microtime(true) * 100000);
-                $this->length[$this->lengthIndex] = $b;
-                $this->lengthIndex++;
-                if ($this->lengthIndex === 8) {
-                    $this->payloadLength = $this->length[0] & 0xff;
-                    $length_length = count($this->length);
-                    for ($i = 1; $i < $length_length; $i++) {
-                        $this->payloadLength = (($this->payloadLength) << 8) | ($this->length[$i] & 0xff);
-                    }
-                    $this->reading = self::MASK;
-                }
-                $end = round(microtime(true) * 100000);
-                echo "LENGTH8 took ".($end - $start)."ms\n";
-                break;
-            case self::MASK:
-                $start = round(microtime(true) * 100000);
-                $this->mask[$this->maskIndex] = $b;
-                $this->maskIndex++;
-                if ($this->maskIndex === 4) {
-                    $this->reading = self::PAYLOAD;
-                    // int l = (int)ByteBuffer.wrap(length).getLong();
-                    //$this->payload = array_fill(0,$this->payloadLength,null);
-                }
-                $end = round(microtime(true) * 100000);
-                echo "MASK took ".($end - $start)."ms\n";
-            break;
-            case self::PAYLOAD:
-                
-                //$payload_length = count($this->payload);
-                if ($this->payloadLength === 0) {
-                    $start = round(microtime(true) * 100000);
-                    //$this->message = new WebSocketCommit($this->payload);
-                    //onMessage(this.message);
-
-                    if(null !== $this->onMessage){
-                        //$payload = implode('',$this->payload);
-                        $payload = '';
-                        $this->listOfPayloads[] = $payload;
-                        if($this->isFinal){
-                            //$res = implode('',$this->listOfPayloads);
-                            $res = '';
-                            $this->listOfPayloads = [];
-                            $this->endTime = round(microtime(true) * 100000);
-                            echo "total time: ".($this->endTime-$this->startTime)."\n";
-                            $this->startTime = 0;
-                            $this->onMessage->run($res);
-                        }
-                    }
-                    
-                    $end = round(microtime(true) * 100000);
-                    echo "PAYLOAD BREAK took ".($end - $start)."ms\n";
-                    break;
-                }
-                /*try {
-                    $this->payload[$this->payloadIndex] = chr(($b ^ $this->mask[$this->payloadIndex % 4]));
-                } catch (\Exception $e) {
-                    echo $e->getTraceAsString()."\n";
-                }*/
-                $this->payloadIndex++;
-                if ($this->payloadIndex === $this->payloadLength) {
-                    echo "here?\n";
-                    $this->reading = self::DONE;
-                    $start = round(microtime(true) * 100000);
-                    
-                    //$this->message = new WebSocketCommit($this->payload);
-                    
-                    if(null !== $this->onMessage){
-                        //$payload = implode('',$this->payload);
-                        $payload = '';
-                        $this->listOfPayloads[] = $payload;
-                        if( $this->isFinal){
-                            //$res = implode('',$this->listOfPayloads);
-                            $res = '';
-                            $this->listOfPayloads = [];
-                            $this->endTime = round(microtime(true) * 100000);
-                            echo "time spent: ".($this->endTime-$this->startTime)."\n";
-                            $this->startTime = 0;
-                            $this->onMessage->run($res);
-                        }
-                    }
-                    $this->lengthKey = 0;
-                    $this->reading = self::FIRST_BYTE;
-                    $this->lengthIndex = 0;
-                    $this->payloadLength = 0;
-                    $this->maskIndex = 0;
-                    $this->payloadIndex = 0;
-                    $this->payload = null;
-                    $this->mask = null;
-                    $this->length = null;
-                    $end = round(microtime(true) * 100000);
-                    echo "DONE took ".($end - $start)."ms\n";
-                }
-            break;
-        }
-    }
 
     private function encodeAndPushBytes(string $messageBytes, bool $binary):void {
         fflush($this->listener->client);
