21444b525e91aeea1705d998dc1350946fe1f7e2
Razvan <tangent.jotey@gmail.com>
2020-11-11 03:52:20 +0100

    Update HttpEventHandler.php

--- src/com/github/tncrazvan/catpaw/http/HttpEventHandler.php
+++ src/com/github/tncrazvan/catpaw/http/HttpEventHandler.php
@@ -1,7 +1,16 @@
 <?php
 namespace com\github\tncrazvan\catpaw\http;
 abstract class HttpEventHandler{
-    protected static function target(string $method, string $path, string $functionName){
+
+    /**
+     * Register a local method as an http event.
+     * @param string $method the http method (in ALL CAPS).
+     * @param string $path the http path (MUST always start with "/").<br />
+     * This path is relative to the path the class itself has been registered to.<br />
+     * @param string $functionName name of the local function.
+     * @return array an array that contains metadata for this registration.
+     */
+    protected static function target(string $method, string $path, string $functionName):array{
         
         $path = \preg_replace('/^\/+/','',$path);
         
@@ -11,8 +20,10 @@ abstract class HttpEventHandler{
             "fname" => $functionName,
         ];
     }
-    public function getHandlerName():string{
-        return get_called_class();
-    }
+
+    /**
+     * Get all registered local methods of this HttpEventHandler.
+     * @return array an array containing all registrations.
+     */
     public abstract static function map():array;
 }
