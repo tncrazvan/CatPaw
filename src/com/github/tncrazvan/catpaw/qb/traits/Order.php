eea0dea932add264cf324f37a0231cafdb02a0d4
Razvan <tangent.jotey@gmail.com>
2021-03-07 17:02:31 +0100

    Update Order.php

--- src/com/github/tncrazvan/catpaw/qb/traits/Order.php
+++ src/com/github/tncrazvan/catpaw/qb/traits/Order.php
@@ -7,28 +7,17 @@ use com\github\tncrazvan\catpaw\qb\tools\QueryConst;
 trait Order{
     /**
      * Add a ORDER BY keyword to the query
+     * @param $name name to order by (column or alias)
+     * @param $order if greater than 0 will force order by "asc", if lesser than 0 will force order by "desc".
      * @return QueryBuilder the QueryBuilder
      */
-    public function orderBy():QueryBuilder{
+    public function orderBy(string $name, int $order = 0):QueryBuilder{
         $this->add(QueryConst::ORDER_BY);
-        return $this;
-    }
-
-    /**
-     * Add a DESC keyword to the query
-     * @return QueryBuilder the QueryBuilder
-     */
-    public function desc():QueryBuilder{
-        $this->add(QueryConst::DESC);
-        return $this;
-    }
-
-    /**
-     * Add a ASC keyword to the query
-     * @return QueryBuilder the QueryBuilder
-     */
-    public function asc():QueryBuilder{
-        $this->add(QueryConst::ASC);
+        $this->add($name);
+        if($order > 0)
+            $this->add(QueryConst::ASC);
+        if($order < 0)
+            $this->add(QueryConst::DESC);
         return $this;
     }
 }
