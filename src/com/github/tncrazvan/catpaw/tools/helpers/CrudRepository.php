4ece44e2612bbafbee4a70101102bc446265cbc9
Razvan <tangent.jotey@gmail.com>
2021-03-07 16:34:45 +0100

    Update CrudRepository.php

--- src/com/github/tncrazvan/catpaw/tools/helpers/CrudRepository.php
+++ src/com/github/tncrazvan/catpaw/tools/helpers/CrudRepository.php
@@ -5,55 +5,67 @@ use com\github\tncrazvan\catpaw\attributes\Inject;
 use com\github\tncrazvan\catpaw\tools\helpers\SimpleQueryBuilder;
 use com\github\tncrazvan\catpaw\qb\tools\Column;
 use com\github\tncrazvan\catpaw\qb\tools\Page;
+use com\github\tncrazvan\catpaw\qb\tools\QueryBuilder;
 use React\Promise\Promise;
 
 class CrudRepository{
-    
+    private ?Page $pg = null;
     #[Inject]
     protected SimpleQueryBuilder $builder;
 
-    public function findAll(?Page $page = null):Promise{
+    public function findAll():Promise{
         $query = 
                 $this
                 ->builder
                 ->select($this->classname);
         
-        if($page)
-            $query->limit(...$page->get());
+        if($this->pg){
+            $query->limit(...$this->pg->get());
+            $this->pg = null;
+        }
 
         return $query->fetchAssoc();
     }
 
-    public function findById(string|int|float|bool|null $id,?Page $page = null):Promise{
+    private function match_pks(QueryBuilder $query, object $model):void{
+        $j = 0;
+        foreach($this->id as &$i){
+            if($j > 0) $query->and();
+            $query->column($i, Column::EQUALS, $model->$i);
+            $j++;
+        }
+    }
+
+    public function page(Page $page):CrudRepository{
+        $this->pg = $page;
+        return $this;
+    }
+
+    public function find(object $object):Promise{
         $query = 
             $this->builder
             ->select($this->classname)
             ->where();
-        $j = 0;
-        foreach($this->id as &$i){
-            if($j > 0) $query->and();
-            $query->column($i, Column::EQUALS, $id);
-            $j++;
-        }
+        
+        $this->match_pks($query,$object);
+        
 
-        if($page)
-            $query->limit(...$page->get());
+        if($this->pg){
+            $query->limit(...$this->pg->get());
+            $this->pg = null;
+        }
 
         return $query->fetchObject($this->classname);
     }
 
-    public function deleteById(string|int|float|bool|null $id):void{
+    public function delete(object $object):void{
         $query = 
         $this
             ->builder
             ->delete($this->classname)
             ->where();
-        $j = 0;
-        foreach($this->id as &$i){
-            if($j > 0) $query->and();
-            $query->column($i, Column::EQUALS, $id);
-            $j++;
-        }
+
+        $this->match_pks($query,$model);
         
         $query->execute(-1);
     }
