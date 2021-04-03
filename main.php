4a6502710457f8386c64ca93b9196c8b3ccf7d44
Razvan <tangent.jotey@gmail.com>
2021-04-03 17:54:47 +0200

    update

--- main.php
+++ main.php
@@ -11,31 +11,32 @@ use React\EventLoop\LoopInterface;
 Factory::setObject(LoopInterface::class,\React\EventLoop\Factory::create());
 
 //make a new simple query builder using a PDO connection and make it injectable
-Factory::setObject(
-    SimpleQueryBuilder::class,new SimpleQueryBuilder(
-        new \PDO(                                       //make the pdo object
-            "mysql:"                                    //dsn
-            ."host=localhost;"
-            ."dbname=todos;"
-            ."port=3306;"
-            ."charset=utf8mb4;",
-            "razshare",                                 //username
-            "razshare"                                  //password
-        ),
-        Factory::make(LoopInterface::class)             //Inject the loop that has just been created above
-    )
-);
+//Factory::setObject(
+//    SimpleQueryBuilder::class,new SimpleQueryBuilder(
+//        new \PDO(                                       //make the pdo object
+//            "mysql:"                                    //dsn
+//            ."host=localhost;"
+//            ."dbname=todos;"
+//            ."port=3306;"
+//            ."charset=utf8mb4;",
+//            "razshare",                                 //username
+//            "razshare"                                  //password
+//        ),
+//        Factory::make(LoopInterface::class)             //Inject the loop that has just been created above
+//    )
+//);
 
 //scan classes
 //using `cat-template`, this will be done automatically for you.
 Factory::make(\examples\Server::class);
 Factory::make(\examples\YieldTest::class);
 Factory::make(\examples\Starter::class);
-Factory::make(\examples\repositories\TaskRepository::class);
-Factory::make(\examples\models\Task::class);
-Factory::make(\examples\DBTasks::class);
+//Factory::make(\examples\repositories\TaskRepository::class);
+//Factory::make(\examples\models\Task::class);
+//Factory::make(\examples\DBTasks::class);
 Factory::make(\examples\ProcessTest::class);
 Factory::make(\examples\ByteRangeTest::class);
+Factory::make(\examples\OpenApi::class);
 
 //create and start server
 $server = new CatPaw(new class extends MainConfiguration{
